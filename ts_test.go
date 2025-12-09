3864a99fd6342befd96b0aa18de10cf3bf218f84
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-09 07:04:21 +0000

    added test with custom error handling

--- ts_test.go
+++ ts_test.go
@@ -2,6 +2,7 @@ package tsserver
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/fs"
 	"testing"
@@ -32,6 +33,10 @@ func (p pseudoFS) Open(name string) (fs.File, error) {
 	}, nil
 }
 
+func errFn(w io.Writer, err error) {
+	fmt.Fprintf(w, "console.log(%q)", err)
+}
+
 func TestWrap(t *testing.T) {
 	for n, test := range [...]struct {
 		Input, Output string
@@ -44,8 +49,12 @@ func TestWrap(t *testing.T) {
 			"const a: number = 1;\n\nconsole.log(a);",
 			"const a/*: number*/ = 1;\n\nconsole.log(a);",
 		},
+		{
+			"const a: = 2;",
+			"console.log(\"ModuleItem: error at position 1 (1:1):\\nStatementListItem: error at position 1 (1:1):\\nDeclaration: error at position 1 (1:1):\\nLexicalDeclaration: error at position 8 (1:8):\\ninvalid lexical declaration\")",
+		},
 	} {
-		f, err := WrapFS(pseudoFS(test.Input)).Open("a.js")
+		f, err := WrapFSWithErrorHandler(pseudoFS(test.Input), errFn).Open("a.js")
 		if err != nil {
 			t.Errorf("test %d: unexpected error while getting file: %s", n+1, err)
 		} else if b, _ := io.ReadAll(f); string(b) != test.Output {
